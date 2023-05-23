95654f7fa0c518b6995186a644a5e24afba87807
linarevicheva <126851225+linarevicheva@users.noreply.github.com>
2023-05-22 23:32:07 -0700

    add mini palette

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Palette from './Palette';
+import PaletteList from './PaletteList';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelper';
 
 class App extends Component {
+
+  findPalette (id) {
+    return seedColors.find(function(palette){
+      return palette.id === id;
+    })
+  }
+
   render () {
     return (
       <div className="App">
-        <Palette palette={generatePalette(seedColors[4])} />
+          <Switch>
+            <Route exact path="/" render={() => <PaletteList palettes={seedColors}/> } />
+            <Route 
+              exact path="/palette/:id" 
+              render={routeProps => (
+                <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id)
+                  )} 
+                /> 
+              )} 
+            />
+          </Switch>
       </div>
     );
   }
